137062bc36dba1cc1585d078bc4c9baa37c7961c
furffico <jerrywang0206@gmail.com>
2023-03-31 13:20:37 +0800

    partial implementation for Adaptive Elitist-AS

--- cvrp/aco.py
+++ cvrp/aco.py
@@ -1,5 +1,8 @@
 import torch
 from torch.distributions import Categorical
+import random
+import itertools
+import numpy as np
 
 CAPACITY = 50
 
@@ -17,20 +20,23 @@ class ACO():
                  pheromone=None,
                  heuristic=None,
                  min=None,
-                 device='cpu'
+                 device='cpu',
+                 adaptive=False,
+                 capacity=CAPACITY
                  ):
         
         self.problem_size = len(distances)
-        self.distances  = distances
-        self.capacity = CAPACITY
+        self.distances = distances
+        self.capacity = capacity
         self.demand = demand
         
         self.n_ants = n_ants
         self.decay = decay
         self.alpha = alpha
         self.beta = beta
-        self.elitist = elitist
+        self.elitist = elitist or adaptive
         self.min_max = min_max
+        self.adaptive = adaptive
         
         if min_max:
             if min is not None:
@@ -46,6 +52,9 @@ class ACO():
                 self.pheromone = self.pheromone * self.min
         else:
             self.pheromone = pheromone
+        
+        if self.adaptive:
+            self.elite_pool = []
 
         self.heuristic = 1 / distances if heuristic is None else heuristic # TODO
 
@@ -58,6 +67,7 @@ class ACO():
         paths, log_probs = self.gen_path(require_prob=True)
         costs = self.gen_path_costs(paths)
         return costs, log_probs
+        
 
     @torch.no_grad()
     def run(self, n_iterations):
@@ -65,17 +75,31 @@ class ACO():
             paths = self.gen_path(require_prob=False)
             costs = self.gen_path_costs(paths)
             
+            if self.adaptive:
+                self.improvement_phase(paths, costs)
+            
+            improved = False
             best_cost, best_idx = costs.min(dim=0)
             if best_cost < self.lowest_cost:
                 self.shortest_path = paths[:, best_idx]
                 self.lowest_cost = best_cost
+                if self.adaptive:
+                    self.intensification_phase()
                 if self.min_max:
                     max = self.problem_size / self.lowest_cost
                     if self.max is None:
                         self.pheromone *= max/self.pheromone.max()
                     self.max = max
-            
-            self.update_pheronome(paths, costs)
+                improved = True
+
+            if not self.adaptive or improved:           
+                self.update_pheronome(paths, costs)
+                if self.adaptive:
+                    self.elite_pool.insert(0, (paths[:, best_idx].clone(), best_cost))
+                    if len(self.elite_pool) > 5:  # pool_size = 5
+                        del self.elite_pool[5:]
+            else:
+                self.diversification_phase()
 
         return self.lowest_cost
        
@@ -90,7 +114,7 @@ class ACO():
         
         if self.elitist:
             best_cost, best_idx = costs.min(dim=0)
-            best_tour= paths[:, best_idx]
+            best_tour = paths[:, best_idx]
             self.pheromone[best_tour[:-1], torch.roll(best_tour, shifts=-1)[:-1]] += 1.0/best_cost
         
         else:
@@ -179,3 +203,193 @@ class ACO():
     
     def check_done(self, visit_mask, actions):
         return (visit_mask[:, 1:] == 0).all() and (actions == 0).all()    
+    
+    # ======== code for adaptive elitist AS ========
+    def get_subroutes(self, route, end_with_zero = True):
+        x = (route == 0).nonzero().flatten()
+        subroutes = []
+        for i, j in zip(x, x[1:]):
+            if j-i>1:
+                if end_with_zero:
+                    j=j+1
+                subroutes.append(route[i:j])
+        return subroutes
+
+    def insertion_single(self, route, index):
+        # route starts from 0, terminates with 0
+        insertion_cost = (((distances[p1, index]+distances[index,p2]-distances[p1, p2]).item(), i) 
+                          for i,(p1,p2) in enumerate(zip(route,route[1:])))
+        min_deltacost, min_index = min(insertion_cost)
+        return min_index, min_deltacost
+    
+    def insertion(self, node_indexes, shuffle = False):
+        route = [node_indexes[0].item()]*2
+        cost = 0
+        if shuffle:
+            perm = torch.randperm(len(node_indexes)-1)+1
+            nodes = node_indexes[perm]
+        else:
+            nodes = node_indexes[1:]
+        for i in nodes:
+            bestpos, deltacost = self.insertion_single(route, i)
+            route.insert(bestpos+1, i.item())
+            cost += deltacost
+        return route, cost
+
+    def merge_subroutes(self, subroutes, length):
+        route = torch.zeros(length, dtype = torch.long, device=self.device)
+        i=0
+        for r in subroutes:
+            if len(r)>2:
+                if isinstance(r, list):
+                    r = torch.tensor(r[:-1])
+                else:
+                    r = r[:-1].clone().detach()
+                route[i: i+len(r)] = r
+                i+=len(r)
+        return route
+    
+    @torch.no_grad()
+    def N1_neighbourhood(self, subroutes, demands, count = 5):
+        # N1 neighbourhood: Pick a random node and insert it in other subroutes.
+        best_insertion = (None, 0.0)
+        for _ in range(count):
+            subroute_index = random.randint(0, len(subroutes)-1)
+            route = subroutes[subroute_index]
+            node_index = random.randint(1, len(route)-2) # exclude depots
+            pred, node, next = route[node_index-1: node_index+2]
+            demand = self.demand[node]
+            avaliable = demands + demand <= self.capacity
+            avaliable[subroute_index] = False
+            if not avaliable.any(): # no avaliable subroute
+                continue
+            cost = self.distances[pred, next] - self.distances[pred, node] - self.distances[node, next]
+            for i, r in itertools.compress(enumerate(subroutes), avaliable):
+                loc, insertion_cost = self.insertion_single(r, node)
+                insertion_cost += cost
+                if insertion_cost < best_insertion[1]:
+                    best_insertion = ((subroute_index, node_index, i, loc+1), insertion_cost)
+
+        if best_insertion[0] is not None: # perform insertion
+            sri, sni, tri, tni = best_insertion[0]
+            subroutes = subroutes[:]
+            source_route, target_route = subroutes[sri], subroutes[tri]
+            node = subroutes[sri][sni]
+            subroutes[tri] = torch.concat([target_route[:tni], node.unsqueeze(0), target_route[tni:]])
+            if len(subroutes[sri])==3:
+                del subroutes[sri]
+            else:
+                subroutes[sri] = torch.concat([source_route[:sni], source_route[sni+1:]])
+            return subroutes, best_insertion[1]
+        else:
+            return best_insertion
+    
+    @torch.no_grad()
+    def N2_neighbourhood(self, subroutes, demands, count = 5):
+        # N2 neighbourhood: Randomly swap 2 nodes and insert them in the best position.
+        best_insertion = (None, 0.0)
+        for _ in range(count):
+            sr1_index, sr2_index = np.random.choice(len(subroutes), size=2, replace=False)
+            sr1, sr2 = subroutes[sr1_index], subroutes[sr2_index]
+            node1_index = random.randint(1, len(sr1)-2)
+            pred1, node1, next1 = sr1[node1_index-1:node1_index+2]
+            demand1 = self.demand[node1]
+            # avaliable nodes to swap
+            avaliable = torch.bitwise_and(
+                demands[sr2_index] + demand1 - self.demand[sr2] <= self.capacity,
+                demands[sr1_index] - demand1 + self.demand[sr2] <= self.capacity,
+            )
+            avaliable[0] = avaliable[-1] = False
+            if not avaliable.any():
+                continue
+            # remove node1 from sr1
+            cost = self.distances[pred1, next1] - self.distances[pred1, node1] - self.distances[node1, next1]
+            sr1_mod = torch.concat([sr1[:node1_index], sr1[node1_index+1:]])
+            # choose a node from sr2
+            avaliable_index = torch.arange(len(sr2))[avaliable]
+            node2_index = np.random.choice(avaliable_index)
+            pred2, node2, next2 = sr2[node2_index-1: node2_index+2]
+            # remove node2 from sr2
+            cost += self.distances[pred2, next2] - self.distances[pred2, node2] - self.distances[node2, next2]
+            sr2_mod = torch.concat([sr2[:node2_index], sr2[node2_index+1:]])
+            # insert node1 into sr2_mod
+            loc1, inscost1 = self.insertion_single(sr2_mod, node1)
+            cost += inscost1
+            sr2_mod = torch.concat([sr2_mod[:loc1+1], node1.unsqueeze(0), sr2_mod[loc1+1:]])
+            # insert node2 into sr1_mod
+            loc2, inscost2 = self.insertion_single(sr1_mod, node2)
+            cost += inscost2
+            sr1_mod = torch.concat([sr1_mod[:loc2+1], node2.unsqueeze(0), sr1_mod[loc2+1:]])
+            if cost < best_insertion[1]:
+                best_insertion = ((sr1_index, sr1_mod, sr2_index, sr2_mod), cost)
+
+        if best_insertion[0] is not None: # perform insertion
+            sr1_index, sr1, sr2_index, sr2 = best_insertion[0]
+            subroutes = subroutes[:]
+            subroutes[sr1_index] = sr1
+            subroutes[sr2_index] = sr2
+            return subroutes, best_insertion[1]
+        else:
+            return best_insertion
+
+    @torch.no_grad()
+    def improvement_phase(self, paths, costs):
+        # local search
+        for i in range(paths.size(1)):
+            subroutes = self.get_subroutes(paths[:, i], end_with_zero=False)
+            # ILS
+            pass
+            # insertion
+            new_subroutes = []
+            new_cost=0
+            for r in subroutes:
+                new_subroute, c = self.insertion(r)
+                new_cost += c
+                new_subroutes.append(new_subroute)
+            if new_cost < costs[i]:
+                paths[:, i] = self.merge_subroutes(new_subroutes, paths.size(0))
+                costs[i] = new_cost
+    
+    @torch.no_grad()
+    def intensification_phase(self):
+        ogroute, ogcost = self.shortest_path, self.lowest_cost
+        subroutes = self.get_subroutes(ogroute, end_with_zero=True)
+        demands = torch.tensor([self.demand[r].sum() for r in subroutes])
+        # print(*subroutes, sep='\n')
+        best_neighbour = (None, 0.0)
+        for func in [self.N1_neighbourhood]: # ,self.N2_neighbourhood]:
+            route, cost = func(subroutes, demands)
+            if cost < best_neighbour[1]:
+                best_neighbour = (route, cost)
+            # if route is not None:
+            #     print(func.__name__, *route, cost, sep='\n')
+        if best_neighbour[0] is not None:
+            self.shortest_path = self.merge_subroutes(best_neighbour[0], self.shortest_path.size(0))
+            self.lowest_cost = self.lowest_cost + best_neighbour[1]
+
+    @torch.no_grad()
+    def diversification_phase(self):
+        # reinitialize pheromone trails
+        self.pheromone = self.pheromone * (self.decay * 0.5) + 0.01
+        for path, cost in self.elite_pool:
+            self.pheromone[path[:-1], torch.roll(path, shifts=-1)[:-1]] += 1.0/cost
+
+
+if __name__=="__main__":
+    from utils import gen_instance
+    n=50
+    demands, distances = gen_instance(n, 'cpu')
+    aco = ACO(
+        distances=distances,
+        demand=demands,
+        adaptive=True
+    )
+    print(aco.run(30))
+    print(aco.shortest_path)
+
+    aco = ACO(
+        distances=distances,
+        demand=demands
+    )
+    print(aco.run(30))
+    print(aco.shortest_path)
