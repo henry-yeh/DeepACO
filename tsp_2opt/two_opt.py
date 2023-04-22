8309ff7f4dc974e4ac17e1126b52a9463e90a2a0
furffy <jerrywang0206@gmail.com>
2023-04-22 15:34:56 +0800

    inference_mode -> no_grad

--- tsp_2opt/two_opt.py
+++ tsp_2opt/two_opt.py
@@ -4,6 +4,7 @@ import numba as nb
 import concurrent.futures
 from functools import partial
 
+@torch.no_grad()
 def batched_two_opt_torch(distances, tour, max_iterations=1000, device="cpu"):
     iterations = 0
     returnnumpy = False
@@ -15,87 +16,87 @@ def batched_two_opt_torch(distances, tour, max_iterations=1000, device="cpu"):
     if isinstance(distances, np.ndarray):
         distances = torch.from_numpy(distances)
 
-    with torch.inference_mode():
-        cuda_dist = distances #.to(device)
-        cuda_tour = tour      #.to(device)
-        batch_size = cuda_tour.shape[0]
-        n = distances.shape[0]
-        min_change = -1.0
-        while min_change < 0.0:
-            points_i = cuda_tour[:, :-1].unsqueeze(-1)
-            points_j = cuda_tour[:, :-1].unsqueeze(1)
-            points_i_plus_1 = cuda_tour[:, 1:].unsqueeze(-1)
-            points_j_plus_1 = cuda_tour[:, 1:].unsqueeze(1)
 
-            A_ij = cuda_dist[points_i, points_j]
-            A_i_plus_1_j_plus_1 = cuda_dist[points_i_plus_1, points_j_plus_1]
-            A_i_i_plus_1 = cuda_dist[points_i, points_i_plus_1]
-            A_j_j_plus_1 = cuda_dist[points_j, points_j_plus_1]
+    cuda_dist = distances #.to(device)
+    cuda_tour = tour      #.to(device)
+    batch_size = cuda_tour.shape[0]
+    n = distances.shape[0]
+    min_change = -1.0
+    while min_change < 0.0:
+        points_i = cuda_tour[:, :-1].unsqueeze(-1)
+        points_j = cuda_tour[:, :-1].unsqueeze(1)
+        points_i_plus_1 = cuda_tour[:, 1:].unsqueeze(-1)
+        points_j_plus_1 = cuda_tour[:, 1:].unsqueeze(1)
 
-            change = A_ij + A_i_plus_1_j_plus_1 - A_i_i_plus_1 - A_j_j_plus_1
-            valid_change = torch.triu(change, diagonal=2)
+        A_ij = cuda_dist[points_i, points_j]
+        A_i_plus_1_j_plus_1 = cuda_dist[points_i_plus_1, points_j_plus_1]
+        A_i_i_plus_1 = cuda_dist[points_i, points_i_plus_1]
+        A_j_j_plus_1 = cuda_dist[points_j, points_j_plus_1]
 
-            min_change = torch.min(valid_change)
-            flatten_argmin_index = torch.argmin(valid_change.reshape(batch_size, -1), dim=-1)
-            min_i = torch.div(flatten_argmin_index, n, rounding_mode='floor')
-            min_j = torch.remainder(flatten_argmin_index, n)
+        change = A_ij + A_i_plus_1_j_plus_1 - A_i_i_plus_1 - A_j_j_plus_1
+        valid_change = torch.triu(change, diagonal=2)
 
-            if min_change < -1e-6:
-                for i in range(batch_size):
-                    cuda_tour[i, min_i[i] + 1:min_j[i] + 1] = torch.flip(cuda_tour[i, min_i[i] + 1:min_j[i] + 1], dims=(0,))
-                iterations += 1
-            else:  
-                break
+        min_change = torch.min(valid_change)
+        flatten_argmin_index = torch.argmin(valid_change.reshape(batch_size, -1), dim=-1)
+        min_i = torch.div(flatten_argmin_index, n, rounding_mode='floor')
+        min_j = torch.remainder(flatten_argmin_index, n)
 
-            if iterations >= max_iterations:
-                break
-        if returnnumpy:
-            # tour = cuda_tour.cpu().numpy()
-            tour = cuda_tour.numpy()
-        else:
-            tour = cuda_tour
-            # tour = cuda_tour.cpu()
+        if min_change < -1e-6:
+            for i in range(batch_size):
+                cuda_tour[i, min_i[i] + 1:min_j[i] + 1] = torch.flip(cuda_tour[i, min_i[i] + 1:min_j[i] + 1], dims=(0,))
+            iterations += 1
+        else:  
+            break
+
+        if iterations >= max_iterations:
+            break
+    if returnnumpy:
+        # tour = cuda_tour.cpu().numpy()
+        tour = cuda_tour.numpy()
+    else:
+        tour = cuda_tour
+        # tour = cuda_tour.cpu()
     return tour, iterations
 
+@torch.no_grad()
 def batched_two_opt_torch_org(points, tour, max_iterations=1000, device="cpu"):
-  ''' source: https://github.com/Edward-Sun/DIFUSCO/blob/main/difusco/utils/tsp_utils.py '''
-  iterator = 0
-  tour = tour.copy()
-  with torch.inference_mode():
+    ''' source: https://github.com/Edward-Sun/DIFUSCO/blob/main/difusco/utils/tsp_utils.py '''
+    iterator = 0
+    tour = tour.copy()
     cuda_points = torch.from_numpy(points).to(device)
     cuda_tour = torch.from_numpy(tour).to(device)
     batch_size = cuda_tour.shape[0]
     min_change = -1.0
     while min_change < 0.0:
-      points_i = cuda_points[cuda_tour[:, :-1].reshape(-1)].reshape((batch_size, -1, 1, 2))
-      points_j = cuda_points[cuda_tour[:, :-1].reshape(-1)].reshape((batch_size, 1, -1, 2))
-      points_i_plus_1 = cuda_points[cuda_tour[:, 1:].reshape(-1)].reshape((batch_size, -1, 1, 2))
-      points_j_plus_1 = cuda_points[cuda_tour[:, 1:].reshape(-1)].reshape((batch_size, 1, -1, 2))
+        points_i = cuda_points[cuda_tour[:, :-1].reshape(-1)].reshape((batch_size, -1, 1, 2))
+        points_j = cuda_points[cuda_tour[:, :-1].reshape(-1)].reshape((batch_size, 1, -1, 2))
+        points_i_plus_1 = cuda_points[cuda_tour[:, 1:].reshape(-1)].reshape((batch_size, -1, 1, 2))
+        points_j_plus_1 = cuda_points[cuda_tour[:, 1:].reshape(-1)].reshape((batch_size, 1, -1, 2))
 
-      A_ij = torch.sqrt(torch.sum((points_i - points_j) ** 2, axis=-1))
-      A_i_plus_1_j_plus_1 = torch.sqrt(torch.sum((points_i_plus_1 - points_j_plus_1) ** 2, axis=-1))
-      A_i_i_plus_1 = torch.sqrt(torch.sum((points_i - points_i_plus_1) ** 2, axis=-1))
-      A_j_j_plus_1 = torch.sqrt(torch.sum((points_j - points_j_plus_1) ** 2, axis=-1))
+        A_ij = torch.sqrt(torch.sum((points_i - points_j) ** 2, axis=-1))
+        A_i_plus_1_j_plus_1 = torch.sqrt(torch.sum((points_i_plus_1 - points_j_plus_1) ** 2, axis=-1))
+        A_i_i_plus_1 = torch.sqrt(torch.sum((points_i - points_i_plus_1) ** 2, axis=-1))
+        A_j_j_plus_1 = torch.sqrt(torch.sum((points_j - points_j_plus_1) ** 2, axis=-1))
 
-      change = A_ij + A_i_plus_1_j_plus_1 - A_i_i_plus_1 - A_j_j_plus_1
-      valid_change = torch.triu(change, diagonal=2)
+        change = A_ij + A_i_plus_1_j_plus_1 - A_i_i_plus_1 - A_j_j_plus_1
+        valid_change = torch.triu(change, diagonal=2)
 
-      min_change = torch.min(valid_change)
-      flatten_argmin_index = torch.argmin(valid_change.reshape(batch_size, -1), dim=-1)
-      min_i = torch.div(flatten_argmin_index, len(points), rounding_mode='floor')
-      min_j = torch.remainder(flatten_argmin_index, len(points))
+        min_change = torch.min(valid_change)
+        flatten_argmin_index = torch.argmin(valid_change.reshape(batch_size, -1), dim=-1)
+        min_i = torch.div(flatten_argmin_index, len(points), rounding_mode='floor')
+        min_j = torch.remainder(flatten_argmin_index, len(points))
 
-      if min_change < -1e-6:
-        for i in range(batch_size):
-          cuda_tour[i, min_i[i] + 1:min_j[i] + 1] = torch.flip(cuda_tour[i, min_i[i] + 1:min_j[i] + 1], dims=(0,))
-        iterator += 1
-      else:
-        break
+        if min_change < -1e-6:
+            for i in range(batch_size):
+                cuda_tour[i, min_i[i] + 1:min_j[i] + 1] = torch.flip(cuda_tour[i, min_i[i] + 1:min_j[i] + 1], dims=(0,))
+            iterator += 1
+        else:
+            break
 
-      if iterator >= max_iterations:
-        break
+        if iterator >= max_iterations:
+            break
     tour = cuda_tour.cpu().numpy()
-  return tour, iterator
+    return tour, iterator
 
 # @jit(nopython = True, parallel = True)
 def batched_two_opt_numpy(dist: np.ndarray, tours: np.ndarray, max_iterations=1000):
